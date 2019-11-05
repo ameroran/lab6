f2fce9096e9259fc2f627e92eeea47ba4505684e
ameroran <oran.amer@yahoo.com>
2019-11-06 00:30:39 +0200

    add text

---
+++ car.js
@@ -0,0 +1,10 @@
+var answer = prompt("Do you love car ( If Yes enter yes if No enter no)")
+var response ;
+
+if (answer == Yes){
+    response = "The cars waiting you"
+} else {
+    response = "You are kidding, right ?"
+}
+
+document.write('<h2>' +response+ '<h2>');
